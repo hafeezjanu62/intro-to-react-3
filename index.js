1afcd6f718af8c6d94383d6f4eb1216a01766ca1
hafeezjanu62 <hafeezjanu62@mail.com>
2022-03-25 17:53:17 +0000

    first commit

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const initializeDBAndServer = async () => {
     });
     app.listen(3000, () => {
       console.log("Server Running at http://localhost:3000/");
+      
     });
   } catch (e) {
     console.log(`DB Error: ${e.message}`);
